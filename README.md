746226240dbc9186c83cf905cfb35bf3f49e135f
Ale Rimoldi <ale@graphicslab.org>
2014-09-11 15:08:12 +0200

    implement most of the features, cleanly separate the parsing from the rendering and document the proejct

--- README.md
+++ README.md
@@ -2,8 +2,112 @@
 
 Generate the documentation from a PHP class.
 
-Parses a classes -- must already be loaded in memory -- for all its methods and renders them in a sane way.
+Parses a class -- must already be loaded in memory -- for all its constants, parameters and methods and documents them in a sane way.
 
-A dozen of todo items have to be implemented before it can be considered usable.
+You can get a clean HTML string or a json string.
 
-No dependency. About 100 LOC. Now. Maybe 150 LOC when it's finished.
+No dependency. Less than 400 LOC.
+
+## Usage
+
+The simplest documentation file is:
+
+~~~ .php
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<?php
+include('ClassToBeDocumented.php');
+include('Documentation.php');
+
+$documentation = new Aoloe\Documentation('ClassToBeDocumented');
+$documentation->parse();
+$documentation->render();
+?>
+</body>
+</html>
+~~~
+
+That's simple:
+- write the HTML "decoration",
+- load the class to be documented (and the `Documentation` framework"),
+- pass its name to the `Documentation` constructor,
+- parse the documentation,
+- render it.
+
+This projects documentation is a _real world_ example, that adds the CSS styling.
+
+## Features
+
+- Uses the PHP built-in reflection classes to get each
+  - constant,
+  - (public) property, and
+  - (public) method.
+- The class and the methods can have doc comments ("JavaDoc") with:
+  - a freeform introductory text,
+  - the type of the (method) parameters (`@var`, `@param`),
+  - the return value of methods (`@return`),
+  - todo lists (`@todo`, `TODO`).
+- Returns
+  - a clean HTML text you can echon on a HTML page, or
+  - a json list you can further process, or
+  - the todo list.
+- Does not differntiate between `protected` and `private`.
+
+Basically, it adds a limited JavaDoc parsing to the PHP own reflection classes.
+
+## Parsing
+
+Doc comments can appear before:
+- the class definition
+- the class variables (parameters)
+- the class methods
+
+The following doc comments are recognized:
+
+- `@var type [description]`
+- `@parm type name [description]`
+- `@return type [description]`
+- `@todo description`
+- `@todo`
+  `- descrition`
+- `TODO description`
+- `TODO`
+  `- descrition`
+
+## Render
+
+The default rendered returns an HTML chunk you can embed in your HTML page.
+
+It uses the following CSS styles:
+
+- `h2`
+- `p.signature`
+- `p.signature span.modifier`
+- `p.signature  span.type`
+- `p.signature  span.name`
+- `p.description`
+
+For a sample, check this project's documentation.
+
+## Json output
+
+You can get the parsed array as json string.
+
+You can also get the list of the todo items as a separate json string.
+
+## Use cases
+
+- Use it as is for small projects that you want to have documented, but where you don't want to pull in a documentation framework that is several order of magnitude bigger than your own code
+- Hack it to get a documentation that perfectly matches the needs for your project.
+
+## Contribute
+
+Contribution are welcome, most of all if they lead to a reduction of the number of lines of code.
+
+There is a todo list at the beginning of the PHP file which contains a list of features planned.
+
+You can also add features request and issues to the GitHub issue tracker. They will be considered, as long as they fit the goals of this project.
--- src/Documentation.php
+++ src/Documentation.php
@@ -2,71 +2,359 @@
 namespace Aoloe;
 
 /**
+ * This is a very simple implemantation of a documentation tool for PHP classes.
+ *
  * TODO:
- * - description can be markdown (class setting)
- * - recognize the @param in the method's doc
+ * - add links and anchors in the rendered version
+ * - description and todo can be markdown (class setting; hook for a filter?)
  * - find out how to show default values for parameters that are arrays
- * - recognize the @return (or catch the $result, where $result can be defined as the default return $value)
- * - separate the static methods
- * - eventually show the public fields
+ * - add options:
+ *   - ignore the non public properties
+ *   - ignore the non public methods
+ *   - do not to render the todos
  * - create links to the github code for each method
- * - show the class constants
- * - create the whole class documentation as a markdown string
- * - collect the TODOs (both oneliners and bullets), remove them from the descriptions and list them
- *   all together (with a reference to the function where they were)
- * - option for output in json or markdown?
- * - maybe parse the file's source for // TODO:...
+ * - create the whole class documentation as a markdown string (through a hook?)
+ * - maybe parse the file's (methods?) source for // TODO:...
  *   (http://stackoverflow.com/questions/7905841/get-source-code-of-user-defined-class-and-functions)
+ * - check https://github.com/peej/phpdoctor
+ * - find a way to initialize and document "structure"
  */
 class Documentation {
     private $reflector = null;
+    private $structure = array (
+        'class' => array (
+            'name' => '',
+            'description' => '',
+            'namespace' => '',
+            'filename' => '',
+        ),
+        'constructor' => array (
+            /*
+            'description' => '',
+            'parameter' => array (
+                'name' => '',
+                'default' => '',
+                'type' => '',
+            ),
+            */
+        ),
+        'method' => array (
+            /*
+            array(
+                'name' => '',
+                'description' => '',
+                'static' => false,
+                'public' => true,
+                'parameter' => array (
+                    'name' => '',
+                    'default' => '',
+                    'type' => '',
+                    'description' => '',
+                ),
+                'return' => array(
+                    'type' => 'void',
+                    'description' => '',
+                ),
+                'deprecate' => 'false',
+            ),
+            */
+        ),
+        'constant' => array (
+            /*
+            array (
+                'name' => '',
+                'value' => '',
+            ),
+            */
+        ),
+        'property' => array (
+            /*
+            array (
+                'name' => '',
+                'description' => '',
+                'static' => false,
+            ),
+            */
+        ),
+        'todo' => array (
+            /*
+            array (
+                'text' => '',
+                'method' => '',
+            ),
+            */
+        ),
+    );
 
     public function __construct($class_name) {
         if (class_exists($class_name)) {
             $this->reflector = new \ReflectionClass($class_name);
         }
     }
-    public function render() {
-        echo('<pre>'.$this->get_comment_cleaned_up($this->reflector->getDocComment()).'</pre>'."\n");
-        $constructor = array();
-        $public = array();
-        $private = array();
-        foreach ($this->reflector->getMethods() as $item) {
-            $method = array (
-                'name' => $item->getName(),
-                'description' => $this->get_comment_cleaned_up($item->getDocComment()),
-                'parameters' => $item->getParameters(),
-            );
-            // echo('<pre>'.print_r($method, 1).'</pre>');
-            if ($item->isConstructor()) {
-                $constructor = $method;
-            } elseif ($item->isPublic()) {
-                $public[] = $method;
-            } else {
-                $private[] = $method;
+    private function get_description_parsed($description) {
+        $result = array (
+            'description' => '',
+            'parameter' => array(),
+            'return' => array(
+                'type' => 'void',
+                'description' => '',
+            ),
+            'todo' => array(),
+        );
+        $section = 'description';
+        foreach (explode("\n", $description) as $item) {
+            // echo("<pre>item:\n".print_r($item, 1)."</pre>\n");
+            $matches = array();
+            preg_match('/^(@return|@param|@var|@todo|TODO:)(.*)/', $item, $matches);
+            // echo("<pre>matches:\n".print_r($matches, 1)."</pre>\n");
+            if (!empty($matches[1])) {
+                switch ($matches[1]) {
+                    case ('@return') :
+                        $section = 'return';
+                    break;
+                    case ('@param') :
+                        $section = 'parameter';
+                    break;
+                    case ('@var') :
+                        $section = 'variable';
+                    break;
+                    case ('@todo') :
+                    case ('TODO:') :
+                        $section = 'todo';
+                        $matches[2] = empty($matches[2]) ? '' : '- '.$matches[2];
+                    break;
+                }
+                $item = empty($matches[2]) ? null : $matches[2];
+            }
+            switch ($section) {
+                case ('description') :
+                    $result['description'] .= (!empty($result['description']) && isset($item) ? "\n" : '').$item;
+                break;
+                case ('return') :
+                    $result['return'] = array (
+                        'type' => strtok(trim($matches[2]), ' '),
+                        'description' => strtok(''),
+                    );
+                break;
+                case ('parameter') :
+                    $type = strtok(trim($matches[2]), ' ');
+                    $name = ltrim(strtok(' '), '$');
+                    // echo("<pre>name:\n".print_r($name, 1)."</pre>\n");
+                    $result['parameter'][$name] = array (
+                        'type' => $type,
+                        'name' => $name,
+                        'description' => strtok(''),
+                    );
+                    // echo("<pre>result[parameter]:\n".print_r($result['parameter'], 1)."</pre>\n");
+                break;
+                case ('variable') :
+                    $result['parameter'] = array (
+                        'type' => strtok(trim($matches[2]), ' '),
+                        'description' => strtok(''),
+                    );
+                break;
+                case ('todo') :
+                    $section = 'todo';
+                    if (isset($item)) {
+                        if (empty($result['todo']) || substr(trim($item), 0, 1) == '-') {
+                            $result['todo'][] = $item;
+                        } else {
+                            $result['todo'][count($result['todo']) - 1] .= "\n".$item;
+                        }
+                    }
+                break;
             }
         }
-        if (!empty($constructor)) {
-            $this->render_method($constructor);
-        }
-        foreach ($public as $item) {
-            $this->render_method($item);
-        }
-        foreach ($private as $item) {
-            $this->render_method($item);
+        // echo("<pre>result:\n".print_r($result, 1)."</pre>\n");
+        return $result;
+    }
+    private function parse_class() {
+        $description = $this->get_comment_cleaned_up($this->reflector->getDocComment());
+        $description = $this->get_description_parsed($description);
+        $this->structure['class'] = array (
+            'name' => $this->reflector->getShortName(),
+            'description' => $description['description'],
+            'namespace' => $this->reflector->getNamespaceName(),
+            'filename' => $this->reflector->getFileName(),
+        );
+        foreach ($description['todo'] as $item) {
+            $this->structure['todo'][] = array (
+                'text' => $item,
+                'method' => null,
+            );
         }
     }
 
-    /**
-     * TODO: transform this into a get_method_string()
-     */
-     
-    private function render_method($method) {
-        $parameters = array();
-        foreach ($method['parameters'] as $item) {
-            $parameters[] = $item->getName().($item->isOptional() ? ' = '.$this->value_to_string($item->getDefaultValue()) : '');
+    private function parse_method() {
+        foreach ($this->reflector->getMethods() as $reflector) {
+            $description = $this->get_comment_cleaned_up($reflector->getDocComment());
+            $description = $this->get_description_parsed($description);
+
+            $parameter = array();
+            foreach ($reflector->getParameters() as $item) {
+                $parameter[$item->getName()] = array (
+                    'name' => $item->getName(),
+                    'default' => $item->isOptional() ? $item->getDefaultValue() : null,
+                    'optional' => $item->isOptional(),
+                    'byReference' => $item->isPassedByReference(),
+                );
+                // echo("<pre>description[parameter']:\n".print_r($description['parameter'], 1)."</pre>\n");
+                $parameter[$item->getName()] += 
+                    array_key_exists($item->getName(), $description['parameter']) ?
+                    $description['parameter'][$item->getName()] :
+                    array ('type' => '');
+            }
+
+            if ($reflector->isConstructor()) {
+                $this->structure['constructor'] = array (
+                    'description' => $description['description'],
+                    'parameter' => $parameter,
+                );
+            } else {
+                $this->structure['method'][$reflector->getName()] = array (
+                    'name' => $reflector->getName(),
+                    'description' => $description['description'],
+                    'static' => $reflector->isStatic(),
+                    'public' => $reflector->isPublic(),
+                    'parameter' => $parameter,
+                    'return' => empty($description['return']) ? 'void' : $description['return'],
+                    'deprecate' => 'false',
+                );
+            }
         }
-        echo('<pre>'.$method['name'].'('.implode(', ', $parameters).')'."\n".$method['description'].'</pre>'."\n");
+        foreach ($description['todo'] as $item) {
+            $this->structure['todo'][] = array (
+                'text' => $item,
+                'method' => $reflector->getName(),
+            );
+        }
+    }
+
+    private function parse_constant() {
+        foreach ($this->reflector->getConstants() as $key => $value) {
+            $this->structure['constant'][$key] = array (
+                'name' => $key,
+                'value' => $value,
+                'type' => gettype($value),
+            );
+        }
+    }
+
+    private function parse_property() {
+        foreach ($this->reflector->getProperties(\ReflectionProperty::IS_PUBLIC) as $reflector) {
+            $description = $this->get_comment_cleaned_up($reflector->getDocComment());
+            $description = $this->get_description_parsed($description);
+            echo("<pre>description:\n".print_r($description, 1)."</pre>\n");
+
+            $this->structure['property'][$reflector->getName()] = array (
+                'name' => $reflector->getName(),
+                'description' => empty($description['parameter']) ? '' : $description['parameter']['description'],
+                'type' => empty($description['parameter']) ? '' : $description['parameter']['type'],
+                'static' => $reflector->isStatic(),
+            );
+        }
+    }
+
+    public function parse() {
+        $this->parse_class();
+        $this->parse_method();
+        $this->parse_constant();
+        $this->parse_property();
+        // echo("<pre>structure:\n".print_r($this->structure, 1)."</pre>\n");
+    }
+
+    public function get_json() {
+        return json_encode($this->structure);
+    }
+
+    public function get_todo_json() {
+        return json_encode($this->structure['todo']);
+    }
+
+    public function render() {
+        $result = "";
+        $result .= "<h1>".$this->structure['class']['name']."</h1>\n";
+        $result .= "<p>".$this->structure['class']['description']."</p>\n";
+        $result .= "<p>Namespace: ".$this->structure['class']['namespace']."</p>\n";
+        $result .= "<p>Filename: ".$this->structure['class']['filename']."</p>\n";
+        if (!empty($this->structure['constant'])) {
+            $result .= "<h2>Constants</h2>\n";
+        }
+        foreach ($this->structure['constant'] as $item) {
+            $result .= "<p>".implode(" ", array (
+                '<span class="modifier">const</span>',
+                '<span class="type">'.$item['type'].'</span>',
+                '<span class="name">'.$item['name'].'</span>',
+                '=',
+                '<span class="name">'.$item['value'].'</span>',
+                )
+            )."</p>\n";
+
+        }
+
+        if (!empty($this->structure['property'])) {
+            $result .= "<h2>Properties</h2>\n";
+        }
+        foreach ($this->structure['property'] as $item) {
+            $result .= '<p class="signature">'.implode(" ", array_filter(array (
+                '<span class="modifier">public</span>',
+                '<span class="type">'.$item['type'].'</span>',
+                '<span class="name">'.$item['name'].'</span>',
+                ))
+            )."</p>\n";
+            if ($item['description'] != '') {
+                $result .= '<p class="description"><span class="description">'.$item['description']."</span></p>\n";
+            }
+
+        }
+
+        if (!empty($this->structure['method'])) {
+            $result .= "<h2>Methods</h2>\n";
+        }
+
+        // show the public methods first
+        usort($this->structure['method'], function($a, $b) {return $b['public'];});
+
+        foreach ($this->structure['method'] as $item) {
+            // echo("<pre>item:\n".print_r($item, 1)."</pre>\n");
+            $result .= '<p class="signature">'.
+                implode(" ", array_filter(array(
+                    '<span class="modifier">'.implode(' ', array_filter(array(
+                        $item['public'] ? 'public' : 'private',
+                        $item['static'] ? 'static' : ''
+                    ))).'</span>',
+                    '<span class="type">'.$item['return']['type'].'</span>',
+                    '<span class="name">'.$item['name'].'</span>',
+                ))).
+                '('.implode(", ", array_filter(
+                    array_map(function($item) { return
+                            ($item['optional'] ? '[' : '').
+                            implode(" ", array_filter(array (
+                            '<span class="type">'.$item['type'].'</span>',
+                            $item['byReference'] ? '&amp;' : ''.
+                            '<span class="name">'.$item['name'].'</span>',
+                            ($item['optional'] ? '= '.$this->value_to_string($item['default']) : '')
+                            )))
+                            .($item['optional'] ? ']' : '')
+                    ;}, $item['parameter'])
+                )).")</p>\n";
+            if ($item['description'] != '') {
+                $result .= '<p class="description"><span class="description">'.$item['description']."</span></p>\n";
+            }
+        }
+
+        if (!empty($this->structure['todo'])) {
+            $result .= "<h2>Todo</h2>\n";
+            $result .= "<ul>\n";
+            foreach ($this->structure['todo'] as $item) {
+                $result .= "<li>".implode(" ", array (
+                    ltrim($item['text'], '- '),
+                    !empty($item['method']) ? '['.$item['method'].'()]' : ''
+                ))."</li>\n";
+            }
+            $result .= "</ul>\n";
+        }
+        echo $result;
     }
 
     private function value_to_string($value) {
@@ -77,8 +365,10 @@ class Documentation {
             $result = $value ? '<true>' : '<false>';
         } elseif (is_string($value)) {
             $result = '"'.$value.'"';
+        } elseif (is_array($value)) {
+            $result = '<array>';
         }
-        return $result;
+        return htmlentities($result);
     }
 
     private function get_comment_cleaned_up($comment) {
